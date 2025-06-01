626af5a51e73f4aca053e8932a1990c7e80c1d12
sameph <samuelephrem2012@gmail.com>
2025-06-01 23:49:34 +0300

    from * to (*any)

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.get("/api/health", (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend/dist")));
-  app.get("/*", (req, res) => {
+  app.get("/(*any)", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
