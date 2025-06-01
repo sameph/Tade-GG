cd3b3cd6026d6a81096098b8f597ba0d78aa09b2
sameph <samuelephrem2012@gmail.com>
2025-06-01 23:54:10 +0300

    removed (*any)

--- server/server.js
+++ server/server.js
@@ -45,9 +45,9 @@ app.get("/api/health", (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend/dist")));
-  app.get("/(*any)", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
+  // app.get("/(*any)", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  // });
 }
 
 
