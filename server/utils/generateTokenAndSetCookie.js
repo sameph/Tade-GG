b994dea78c6c8662f36e88b6ee88fe5b038aa698
sameph <samuelephrem2012@gmail.com>
2025-06-06 16:21:53 +0300

    reset to previous state and set cooie to one day

--- server/utils/generateTokenAndSetCookie.js
+++ server/utils/generateTokenAndSetCookie.js
@@ -2,14 +2,14 @@ import jwt from "jsonwebtoken";
 
 export const generateTokenAndSetCookie = (res, user) => {
 	const token = jwt.sign({ user }, process.env.JWT_SECRET, {
-		expiresIn: "7d",
+		expiresIn: "1d",
 	});
 
 	res.cookie("token", token, {
 		httpOnly: true,
 		secure: process.env.NODE_ENV === "production",
 		sameSite: "strict",
-		maxAge: 7 * 24 * 60 * 60 * 1000,
+		maxAge: 1 * 24 * 60 * 60 * 1000,
 	});
 
 	return token;
